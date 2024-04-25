c37213babd320015198858a948e5e5960b901c5b
A-Little-Excited <3030036858@qq.com>
2024-04-25 15:14:49 +0800

    ci/cd

--- target/apidocs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"com.paiondata","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"com.paiondata","c":"App","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();

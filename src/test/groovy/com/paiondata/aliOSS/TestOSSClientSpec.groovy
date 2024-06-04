2ca724cc5ff7835e1970f62a62f4ec797467e800
A-Little-Excited <3030036858@qq.com>
2024-06-04 14:17:42 +0800

    add-test

--- src/test/groovy/com/paiondata/aliOSS/TestOSSClientSpec.groovy
+++ src/test/groovy/com/paiondata/aliOSS/TestOSSClientSpec.groovy
@@ -20,5 +20,25 @@ class TestOSSClientSpec extends Specification{
 
         cleanup:
         expectInputStream.close()
+        actualInputStream.close()
+    }
+
+    def "Download the file repeatedly"() {
+        given: "mock fileContent to upload and testClient"
+        final InputStream expectInputStream = new ByteArrayInputStream("test".getBytes())
+        TestOSSClient testOSSClient = new TestOSSClient()
+
+        when: "upload file by using TestOSSClient"
+        testOSSClient.putObject(BUCKET_NAME, FILE_ID, expectInputStream)
+
+        then: "download the file repeatedly and compare the file content"
+        InputStream firstInputStream = testOSSClient.getObject(BUCKET_NAME, FILE_ID).getObjectContent()
+        InputStream secondInputStream = testOSSClient.getObject(BUCKET_NAME, FILE_ID).getObjectContent()
+        firstInputStream.getText() == secondInputStream.getText()
+
+        cleanup:
+        expectInputStream.close()
+        firstInputStream.close()
+        secondInputStream.close()
     }
 }
